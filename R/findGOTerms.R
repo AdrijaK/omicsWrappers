f56df7d59843e4346b8419e4061b844ce93f46f0
Adrija Kalvisa <qlr900@briclinux04.sund.root.ku.dk>
2020-05-25 15:17:10 +0200

    add new functions

---
+++ R/findGOTerms.R
@@ -0,0 +1,31 @@
+#' findGOTerms
+#' Find a list of GO terms based on keywords based on constraints "A AND B"
+#' Finds a list of GO terms based on two keywords in the `MouseMine` using `InterMineR` library
+#' @param KeywordsVector a character string vector of length 2
+#' @example
+#' findGOTerms(c("histone", "H3-K4"))
+
+findGOTerms = function(KeywordsVector){
+  library(InterMineR)
+
+  # load MouseMine
+  im = initInterMine(mine=listMines()["MouseMine"])
+
+  # Get template (collection of pre-defined queries)
+  template = getTemplates(im)
+
+  # Get GO-terms related templates
+  template[grep("go", template$name, ignore.case=TRUE),]
+
+  # Lookup_GO Returns GO terms that match a specified search string:
+  queryLookupGO = getTemplateQuery(im, "Lookup_GO")
+
+  # modify the lookup terms so that the query looks for GO terms that contain "histone" AND "H3-K4" (max 2 terms for the default)
+  queryLookupGO$where[[1]][["value"]] = KeywordsVector[1]
+  queryLookupGO$where[[2]][["value"]] = KeywordsVector[2]
+
+  # run the query. Query returns a `data.frame` object with GO terms that match the query terms
+  output = runQuery(im, queryLookupGO)
+
+  return(output)
+}
